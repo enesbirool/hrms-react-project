ec856e2e54385a3d12fdabe027e2408c7f37dc49
mXBozkurt <biroltaekwondo43@gmail.com>
2021-06-11 00:07:33 +0300

    11.06.2021/00:07

--- src/layouts/Section.jsx
+++ src/layouts/Section.jsx
@@ -4,6 +4,7 @@ import { Grid, GridColumn } from "semantic-ui-react";
 import JobseekerList from "../pages/JobSeekerList";
 import JobSeekerDetail from "../pages/JobSeekerDetail";
 import EmployerList from "../pages/EmployerList";
+import JobPositionList from "../pages/JobPositionList";
 
 export default function Section() {
   return (
@@ -15,6 +16,7 @@ export default function Section() {
             <Route exact path="/jobseekers" component={JobseekerList} />
             <Route exact path="/jobseeker/:id" component={JobSeekerDetail} />
             <Route exact path="/employers" component={EmployerList} />
+            <Route exact path="/jobpositions" component={JobPositionList} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
